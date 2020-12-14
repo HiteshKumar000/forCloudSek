4709b6a9ec32ef9a75814eb2ba973911aa6ee1a6
HiteshKumar000 <75985082+HiteshKumar000@users.noreply.github.com>
2020-12-14 18:04:39 +0530

    Add files via upload
    please create a txt file with required inputs

---
+++ Main.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+import java.io.*;  
+
+public class Main {
+
+    public static void getHTTPResponseStatusCode(String websiteUrl) throws IOException {
+	URL url = new URL(websiteUrl);
+        HttpURLConnection http = (HttpURLConnection)url.openConnection();
+	if(http.getResponseCode()<400) 	
+	{
+	System.out.print(" "+http.getResponseCode());
+	}
+    }
+    public static void main(String [] as) throws IOException
+    {	
+	Scanner obj=new Scanner(System.in);
+	System.out.println("Please enter the website url");
+	String websiteUrl=obj.next();
+	obj.close();
+	try  
+	{  
+	FileInputStream fis=new FileInputStream("urlfile.txt");       
+	Scanner sc=new Scanner(fis);	
+	System.out.print("Successful code are:[");     
+	while(sc.hasNextLine())  
+	{ 
+	getHTTPResponseStatusCode(websiteUrl+"/"+sc.nextLine());      
+	}  
+	sc.close();       
+	}  
+	catch(Exception e)  
+	{  
+	e.printStackTrace();  
+	}  
+	System.out.print(" ]");
+        
+    }
+}
